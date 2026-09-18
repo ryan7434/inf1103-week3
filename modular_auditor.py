d993200848f904395243bfc2e9ddae8d0bc7cb49
Your Name <you@example.com>
2026-09-18 13:17:15 +0800

    fifth commit

--- modular_auditor.py
+++ modular_auditor.py
@@ -28,6 +28,35 @@ def generate_reports(total_units,failed_attempts):
     print("Total units processed:", total_units)
     print("Failed entries:", failed_attempts)
 
+def main():
+    inventory = 0
+    failed_entries = 0
+
+    while True:
+        result = get_valid_input()
+
+        if result == "quit":
+            break
+
+        elif result is None:
+            failed_entries += 1
+            continue
+
+        inventory = process_delivery(inventory, result)
+
+        if inventory > 500:
+            print("Inventory limit exceeded. Cannot add more stock.")
+            break
+
+    tax_amount = calculate_tax(inventory)
+    print("Total tax on inventory:", tax_amount)
+
+    generate_reports(inventory, failed_entries)
+
+main()
+#if __name__ == "__main__":
+    #main()
+
 
 
 
