d25428c62fe6ef1755cc4b27bced4186915880e4
Your Name <you@example.com>
2026-09-18 13:16:39 +0800

    third commit

--- modular_auditor.py
+++ modular_auditor.py
@@ -20,6 +20,10 @@ def process_delivery(current_total, new_value):
     new_total = current_total + new_value
     return new_total
 
+def calculate_tax(amount):
+    tax = amount * 0.10
+    return tax
+
 
 
 
